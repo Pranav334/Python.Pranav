da6cdfc9a82ad932dbb2b7e541f1d4afb5cb2565
Pranav <114337347+Pranav334@users.noreply.github.com>
2022-12-01 21:03:19 +0530

    Add files via upload

---
+++ Class3- Print Command Functionalities-CipherSchool.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 18:44:59 2022
+
+@author: prana
+"""
+
+print("Pranav Prakash")
+print(1)
+print(1,2,3,"pranav","5+2j")
+print("Hello",end=";")
+print("World")
+print("a")
+print("b")
+print("c")
