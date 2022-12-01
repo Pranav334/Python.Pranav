91029bba5ee4a0f4042f48bc209f95fce65043b3
Pranav <114337347+Pranav334@users.noreply.github.com>
2022-12-01 21:02:43 +0530

    Add files via upload

---
+++ Class2-Basics of Python - Primitive Data Types, Comments, Variables -Cipher School.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 18:22:52 2022
+
+@author: prana
+"""
+
+print("Hello World")
+m = 'a'
+print(type(m))
+a = 255
+print(id(a))
+print(hex(id(a)))
+b = 200
+print(id(a))
+c = 278
+print(id(c))
+a = 1323244327893754759
+print(a+1)
