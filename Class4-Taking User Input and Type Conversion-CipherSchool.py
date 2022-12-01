b888883775af49c1577bb718e70e09f0542352b9
Pranav <114337347+Pranav334@users.noreply.github.com>
2022-12-01 21:03:37 +0530

    Add files via upload

---
+++ Class4-Taking User Input and Type Conversion-CipherSchool.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 18:58:01 2022
+
+@author: prana
+"""
+
+a = 123
+b = 245
+a = input()
+b = input()
+print(a)
+print(b)
+
+int(a) = 65
+char(b) = char(a);
+print(bin(65))
